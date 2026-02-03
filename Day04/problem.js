efd4d4b448a975f56cbec07ebb9cd60dbf4da3ba
alirazagpt4 <ali.4aug24webgpt@gmail.com>
2026-02-03 13:10:40 +0500

    Day 4 done and code submitted.

---
+++ Day04/problem.js
@@ -0,0 +1,39 @@
+// factorial   ka matlb hai kisi bhi number se shuru ho kr 1 tk wapis jana 
+// aur raste main ana wala sara numbers se mutliply krna 
+let n = 5;
+
+    let fact = 1;
+
+for (let i = 1 ; i <= n ; i++){
+  fact = fact * i;
+}
+
+console.log(fact);
+
+// fibonacci series ek sequence ki series hai mtlb ke har naya number pichla 2 numbers ka sum
+// ho ke banta hai 
+// sequence dekhna hai 5 ,10 ,15 , 20 , 25 ....... 
+let number = 100;
+let a = 5;
+let b = 10;
+let c = 0;
+
+while(c<=number){
+  c = a + b;
+   
+  if(c == number){
+    console.log("we get the sequence number", c);
+    break;
+  }
+  else{
+    a = b;
+    b = c;
+  }
+  
+ 
+}
+
+if(c != number){
+    console.log("number is not the part of sequnce :" , c);
+  
+  }
